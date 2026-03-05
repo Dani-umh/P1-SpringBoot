5d42b9337848457a054f264bdfe6bdb75e6ddb0e
Daniel Garcia Moreno <daniel.garcia34@goumh.umh.es>
2026-03-05 21:07:44 +0100

    Add even form submission and result page

--- src/main/java/demoapp/controller/EvenFormController.java
+++ src/main/java/demoapp/controller/EvenFormController.java
@@ -1,15 +1,45 @@
 package demoapp.controller;
 
+import demoapp.service.EvenService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class EvenFormController {
+
+    @Autowired
+    private EvenService evenService;
 
     @GetMapping("/evenform")
     public String showEvenForm(Model model) {
         model.addAttribute("evenData", new EvenData());
         return "evenForm";
+    }
+
+    @PostMapping("/evenform")
+    public String processEvenForm(@Valid EvenData evenData,
+                                  BindingResult bindingResult,
+                                  Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("evenData", evenData);
+            return "evenForm";
+        }
+
+        boolean result = evenService.isEven(evenData.getNumber());
+
+        if (result) {
+            model.addAttribute("message", evenData.getNumber() + " is even");
+        } else {
+            model.addAttribute("message", evenData.getNumber() + " is NOT even");
+        }
+
+        return "evenResult";
     }
 }
