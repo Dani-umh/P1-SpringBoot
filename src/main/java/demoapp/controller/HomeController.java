93117fa39ac5b455fd97ca5caed9b1d5f4685b27
Daniel Garcia Moreno <daniel.garcia34@goumh.umh.es>
2026-03-05 23:14:39 +0100

    Add homepage menu with links to forms

--- src/main/java/demoapp/controller/HomeController.java
+++ src/main/java/demoapp/controller/HomeController.java
@@ -1,14 +1,13 @@
 package demoapp.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public @ResponseBody String greeting() {
-        return "Hello World, my name is Daniel Garcia Moreno";
+    @GetMapping("/")
+    public String home() {
+        return "home";
     }
 }
