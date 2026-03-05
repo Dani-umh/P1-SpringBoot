5dfa0422842775e29df37c227704ffda99cedf14
Daniel Garcia Moreno <daniel.garcia34@goumh.umh.es>
2026-03-05 23:34:59 +0100

    Minor updates

--- src/main/java/demoapp/controller/PalindromeData.java
+++ src/main/java/demoapp/controller/PalindromeData.java
@@ -1,4 +1,19 @@
 package demoapp.controller;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class PalindromeData {
+
+    @NotBlank(message = "The word cannot be empty")
+    @Size(min = 2, max = 50, message = "The word must be between 2 and 50 characters")
+    private String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
 }
